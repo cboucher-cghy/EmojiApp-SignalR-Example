4e9bf64e780e21f9de93958f6dafd5cb1f3ea700
Charles Sarrazin-Boucher <cboucher@cegepgranby.qc.ca>
2024-10-15 12:44:34 -0400

    Unused parameters in VITE

--- emojiapp.client/vite.config.js
+++ emojiapp.client/vite.config.js
@@ -10,31 +10,31 @@ import { env } from 'process';
 import vue from "@vitejs/plugin-vue";
 
 
-const baseFolder =
-    env.APPDATA !== undefined && env.APPDATA !== ''
-        ? `${env.APPDATA}/ASP.NET/https`
-        : `${env.HOME}/.aspnet/https`;
+//const baseFolder =
+//    env.APPDATA !== undefined && env.APPDATA !== ''
+//        ? `${env.APPDATA}/ASP.NET/https`
+//        : `${env.HOME}/.aspnet/https`;
 
-const certificateName = "emojiapp.client";
-const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+//const certificateName = "emojiapp.client";
+//const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+//const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
-    }
-}
+//if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+//    if (0 !== child_process.spawnSync('dotnet', [
+//        'dev-certs',
+//        'https',
+//        '--export-path',
+//        certFilePath,
+//        '--format',
+//        'Pem',
+//        '--no-password',
+//    ], { stdio: 'inherit', }).status) {
+//        throw new Error("Could not create certificate.");
+//    }
+//}
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7222';
+//const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
+//    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7222';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -51,19 +51,19 @@ export default defineConfig(({ mode }) => {
                 '@': fileURLToPath(new URL('./src', import.meta.url))
             }
         },
-        server: {
-            proxy: {
-                '^/weatherforecast': {
-                    target,
-                    secure: false
-                }
-            },
-            port: 5173,
-            https: {
-                key: fs.readFileSync(keyFilePath),
-                cert: fs.readFileSync(certFilePath),
-            }
-        },
+        //server: {
+        //    proxy: {
+        //        '^/weatherforecast': {
+        //            target,
+        //            secure: false
+        //        }
+        //    },
+        //    port: 5173,
+        //    https: {
+        //        key: fs.readFileSync(keyFilePath),
+        //        cert: fs.readFileSync(certFilePath),
+        //    }
+        //},
         base: env.NODE_ENV == "production" ? "/cboucher" : "/",
         build: {
             sourcemap: true
